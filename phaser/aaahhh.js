4a50d20743e535efef13b7bbcde061cd4a6e5f02
wilIump <41800466+wilIump@users.noreply.github.com>
2018-12-17 14:13:36 -0500

    Update aaahhh.js
    fixing the fix to fix the fix

--- phaser/aaahhh.js
+++ phaser/aaahhh.js
@@ -87,10 +87,10 @@ function update() {
     game.physics.arcade.overlap(sword, enemy2, sword_hide);
     game.physics.arcade.overlap(sword, enemy3, sword_hide);
     game.physics.arcade.overlap(sword, enemy4, sword_hide);
-    game.physics.arcade.overlap(enemy1, sword, enemy_hide);
-    game.physics.arcade.overlap(enemy2, sword, enemy_hide);
-    game.physics.arcade.overlap(enemy3, sword, enemy_hide);
-    game.physics.arcade.overlap(enemy4, sword, enemy_hide);
+    game.physics.arcade.overlap(enemy1, sword, enemy_hide(enemy1));
+    game.physics.arcade.overlap(enemy2, sword, enemy_hide(enemy2));
+    game.physics.arcade.overlap(enemy3, sword, enemy_hide(enemy3));
+    game.physics.arcade.overlap(enemy4, sword, enemy_hide(enemy4));
     player.body.velocity.set(0);
 
     attack();
@@ -159,8 +159,8 @@ function attack() {
 function sword_hide(){
     sword.visible = false;
 }
-function enemy_hide(){
-    this.visible = false;
+function enemy_hide(enemy){
+    enemy.visible = false;
 }
 function spawn_enemy(){
     /*
